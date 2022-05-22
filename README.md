7d02b80ebf66d9132e276e79c5277225402a0963
sandertn <sandertn@hotmail.no>
2022-05-22 15:01:26 +0200

    removed stuff

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # TEK5030_project
 
+## How to match images with maps
+To install the required libraries to run our code, please use 
+```
+pip install -r requirements.txt
+```
+
+Our main file is merge_images.py. Please run this file to see the results. If you want to change images and maps being merged, please change filenames in the code. 
+
+Please note that some files are deprecated versions of old code in this folder, and some files are required to make SuperGlue work. 
+
+The report is also found in this zipped folder. Please refer to it for details on the project. 
+
 ## How to install OSMnx
 This is used to download, extract and show roads from maps from OpenStreetMap.
-Be in your chosen virtual conda environment.
+Be in your chosen virtual Conda environment.
 ```
 conda config --prepend channels conda-forge
 conda install osmnx
--- merge_images.py
+++ merge_images.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
-import image_plotter
+#import image_plotter
 import matplotlib.pyplot as plt
 import networkx as nx
-import map_segmentation
-import osmnx as ox
+#import map_segmentation
+#import osmnx as ox
 import get_keypoint_matches as gkm
+import os 
 
 
 def get_affine_transformation(img, pts1, pts2):
@@ -51,103 +52,14 @@ def image_to_graph(img:np.ndarray) -> nx.MultiGraph:
     G = nx.from_numpy_matrix(img, create_using=nx.MultiGraph)
     return G
 
-def main6():
-    img = cv2.imread("dataset/map_segmented_roads/blindern_roads.png")
-    plot = True
 
-    # Rotate
-    pts1 = np.float32([[55,55],[355,55],[555,350],[1550,1650]])
-    pts2 = np.float32([[0,0],[300,0],[500,300],[1500,1600]])
-    rotated = rotate_crop_img(img, pts1, pts2)
-
-    if plot:
-        plt.subplot(121)
-        plt.imshow(img)
-        plt.title('Input')
-        plt.subplot(122)
-        plt.imshow(rotated)
-        plt.title('Rotated')
-        plt.show()
-
-    # Unrotate 
-    x, y, *_ = rotated.shape
-    rotated_boarder = cv2.rectangle(rotated, (0,0), (x,y), color=(255, 0, 0), thickness=30)
-    unrotate = np.zeros(img.shape, dtype=np.uint8)
-    unrotate[550:1300, 900:1650] = rotated_boarder.copy()
-    unrotate = get_affine_transformation(unrotate, pts2, pts1)
-
-    if plot:
-        plt.subplot(121)
-        plt.imshow(img)
-        plt.title('Input')
-        plt.subplot(122)
-        plt.imshow(unrotate)
-        plt.title('Unrotated')
-        plt.show()
-
-    overlay = image_plotter.get_overlay_img(img, unrotate)
-    plt.imshow(overlay)
-    plt.show()
-
-
-def main1():
-    img = cv2.imread("dataset/map_segmented_roads/blindern_roads.png")
-    graph = image_to_graph(img)
-    graph = nx.Graph(graph)
-
-    center_point = (59.9433832, 10.727962) # Blindern
-    dist = 800
-    G = map_segmentation.get_road_img_from_center_point(center_point, dist=dist, edge_linewidth=2.0, show=False)
-    G = nx.Graph(G)
-
-    print(nx.faster_could_be_isomorphic(G, G.copy()))
-
-
-def main3():
-    ifi = (59.9435754, 10.7181494)
-    dist = 200
-    img_ifi = cv2.imread("dataset/satellite/blindern_ifi.png")
+def get_overlay_img(img1:np.ndarray, img2:np.ndarray, alpha:int=0.5) -> np.ndarray:
+    overlay = img1.copy()
+    output = img2.copy()
+    overlap = np.where(output > 0, output, overlay)
     
-    G = map_segmentation.get_road_img_from_center_point(center_point=ifi, dist=dist, edge_linewidth=2.0, show=False, road_type="drive")
-    buildings = map_segmentation.get_buildings_from_center_point(center_point=ifi, dist=dist)
-    nodes, edges = ox.graph_to_gdfs(G)
-
-    
-    ax1 = plt.subplot(121)
-    ax1.set_facecolor("black")
-    buildings.plot(ax=ax1, facecolor="khaki", alpha=1.0,)
-    edges.plot(ax=ax1, linewidth=2, edgecolor="white")
-    plt.title('ifi')
-
-    ax2 = plt.subplot(122)
-    ax2.imshow(img_ifi)
-    plt.title('Roads')
-    plt.show()
-    
-    pts1 = np.float32([
-        [10.721105, 59.9441596],
-        [10.718055, 59.944496],
-        [10.722278, 59.943155],
-        [10.719949, 59.944371]])
-    pts2 = np.float32([
-        [995.0, 290.0],
-        [504.7, 120.0],
-        [1208.6, 632.1],
-        [850.3, 200.0]])
-
-    minx, miny, maxx, maxy = edges.total_bounds
-    x_diff = int(maxx - minx)
-    y_diff = int(maxy - miny)
-
-    y, x, *_ = img_ifi.shape
-    rotated_boarder = cv2.rectangle(img_ifi, (0,0), (x,y), color=(255, 0, 0), thickness=30)
-    unrotate = np.zeros((x_diff, y_diff, 3), dtype=np.uint8)
-    print(unrotate.shape)
-    unrotate[11378-825:11378, 7807-1440:7807] = rotated_boarder.copy()
-    unrotate = get_affine_transformation(unrotate, pts2, pts1)
-    
-    plt.imshow(unrotate)
-    plt.show()
+    dst = cv2.addWeighted(overlap, 0.7, overlay, 0.3, 0)
+    return dst
 
 
 def merge_images(img1, img2, match_points):
@@ -175,87 +87,17 @@ def merge_images(img1, img2, match_points):
     unrotate[0:x, 0:y] = smallest_img.copy()
     unrotate_warped = get_homography_transformation(unrotate, img2_match, img1_match) #get_affine_transformation(unrotate, img2_match, img1_match)
 
-    overlay = image_plotter.get_overlay_img(largest_img, unrotate_warped)
+    overlay = get_overlay_img(largest_img, unrotate_warped)
     #plt.imshow(overlay)
     #plt.show()
     return overlay
 
-def main4():
-    kjeller_fly_medium = cv2.imread("dataset/map/kjeller_kart_ny.png", )
-    kjeller_kart_lite = cv2.imread("dataset/map/kjeller_flyfoto_ny.png")
-
-    kjeller_fly_medium = cv2.cvtColor(kjeller_fly_medium, cv2.COLOR_BGR2RGB)
-    kjeller_kart_lite = cv2.cvtColor(kjeller_kart_lite, cv2.COLOR_BGR2RGB)
-
-    match_points = gkm.get_matchpoints(kjeller_fly_medium, kjeller_kart_lite)
-    print("----------------")
-    print(match_points)
-
-    if True:
-        colours = ["*b", "*g", "*r", "*m"]
-        plt.subplot(121)
-        plt.imshow(kjeller_fly_medium)
-        plt.title('kjeller_fly_medium')
-        for i in range(len(match_points)):
-            plt.plot(match_points[i][0][0], match_points[i][0][1], colours[i])
-        plt.subplot(122)
-        plt.imshow(kjeller_kart_lite)
-        plt.title('kjeller_kart_lite')
-        for i in range(len(match_points)):
-            plt.plot(match_points[i][1][0], match_points[i][1][1], colours[i])
-        plt.show()
-
-    # (kjeller_flyfoto_medium, kjeller_kart_lite)
-    # ([1002., 518.], [258., 634.])
-    # ([1064., 661.], [326., 777.]),
-    # ([757., 684.], [13., 797.]),  
-    # ([1081., 557.], [337., 672.]),
-    # ([984., 101.], [240., 220.]),,
-
-    match_points = [
-                ([1370., 876.], [629., 987.]),
-                ([760., 142.], [28., 256.]),
-                ([753., 750.], [16., 870.]),
-                ([1247., 260.], [504., 376.])
-            ]
-
-
-    fly_match = []
-    map_match = []
-
-    for matches in match_points:
-        point1, point2 = matches
-        fly_match.append(point1)
-        map_match.append(point2)
-
-    fly_match = np.array(fly_match, dtype=np.float32)
-    map_match = np.array(map_match, dtype=np.float32)
-
-    x, y, *_ = kjeller_kart_lite.shape
-    #rotated_boarder = cv2.rectangle(kjeller_kart_lite, (0,0), (x,y), color=(255, 0, 0), thickness=30)
-    unrotate = np.zeros(kjeller_fly_medium.shape, dtype=np.uint8)
     
-    x, y, *_ = kjeller_kart_lite.shape
-    unrotate[0:x, 0:y] = kjeller_kart_lite.copy()
-    kjeller_kart_lite_warp = get_affine_transformation(unrotate, map_match, fly_match)
-
-    print(kjeller_fly_medium.shape, kjeller_kart_lite_warp.shape)
-    if True:
-        plt.subplot(121)
-        plt.imshow(kjeller_fly_medium)
-        plt.title('Input')
-        plt.subplot(122)
-        plt.imshow(kjeller_kart_lite_warp)
-        plt.title('Unrotated')
-        plt.show()
-
-    overlay = image_plotter.get_overlay_img(kjeller_fly_medium, kjeller_kart_lite_warp, alpha=0.7)
-    plt.imshow(overlay)
-    plt.show()
-    
-
 
 def main5():
+    """
+    deprecated main used for testing
+    """
     # match_points = [
     #             ([1370., 876.], [629., 987.]),
     #             ([760., 142.], [28., 256.]), 
@@ -318,10 +160,7 @@ def main():
     plt.imsave("overlay_homography_all_points.png", overlay, dpi=400.)
 
 if __name__ == "__main__":
-    # main6()
-    # main1()
-    # main3()
-    # main4()
+    os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
     # main5()
     main()
     
